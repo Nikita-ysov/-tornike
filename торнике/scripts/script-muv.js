378d0bca38a531d3a759b52710584ca6d22878b2
Nikita-ysov <156422968+Nikita-ysov@users.noreply.github.com>
2025-04-14 13:47:28 +0500

    Add files via upload

--- торнике/scripts/script-muv.js
+++ торнике/scripts/script-muv.js
@@ -1,6 +1,22 @@
 document.getElementById('burger-menu').addEventListener('click', function() {
-    
     const navList = document.querySelector('.navigation-list');
     
-    navList.classList.toggle('hidden');
+    // Проверяем, есть ли класс 'hidden'
+    if (navList.classList.contains('hidden')) {
+        navList.classList.remove('hidden'); // Убираем класс 'hidden', чтобы открыть меню
+    } else {
+        navList.classList.add('hidden'); // Добавляем класс 'hidden', чтобы закрыть меню
+    }
+});
+document.querySelector('.button-mu').addEventListener('click', function() {
+    const hiddenItems = document.querySelectorAll('.product-card-m.hidden');
+
+    if (hiddenItems.length > 0) {
+        hiddenItems.forEach(item => {
+            item.classList.remove('hidden');
+        });
+        
+        
+        this.style.display = 'none'; 
+    }
 });
--- торнике/scripts/script.js
+++ торнике/scripts/script.js
@@ -1,6 +1,10 @@
 document.getElementById('burger-menu').addEventListener('click', function() {
-    
     const navList = document.querySelector('.navigation-list');
     
-    navList.classList.toggle('hidden');
+    // Проверяем, есть ли класс 'hidden'
+    if (navList.classList.contains('hidden')) {
+        navList.classList.remove('hidden'); // Убираем класс 'hidden', чтобы открыть меню
+    } else {
+        navList.classList.add('hidden'); // Добавляем класс 'hidden', чтобы закрыть меню
+    }
 });
